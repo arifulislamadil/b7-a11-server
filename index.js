dc444782b3bcfb6f814b35fedcfdbff8227aece8
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-05-24 13:21:08 +0400

    add joson file

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const cors = require('cors')
+const products = require("./products.json")
+
+// app.use(express.json())
+app.use(cors())
+//products list 
+
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.send('hello world')
 })
+app.get('/products', (req, res) => {
+  res.send(products)
+})
+app.get('/product/:productId', (req, res) => {
+  const id = req.params.productId;
+  res.json(products[id])
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
