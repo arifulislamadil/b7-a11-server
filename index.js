5c6e02c95441f343beee4a4bf1c88a6b234d2d64
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-05-24 15:04:46 +0400

    find single toy from database

--- index.js
+++ index.js
@@ -3,22 +3,70 @@ const app = express()
 const port = 5000
 const cors = require('cors')
 const products = require("./products.json")
+require("dotenv").config()
 
 // app.use(express.json())
 app.use(cors())
+app.use(express.json())
 //products list 
+
+// mongodb configuration
+
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ksrt5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toysCollection = client.db('toys').collection('toysList');
+
+   app.get("/products",async(req,res) => {
+    const cursor = toysCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
+   app.get("/product/:id",async(req,res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await toysCollection.findOne(query);
+    // const result = await cursor.toArray();
+    res.send(result);
+   })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// ........................
+
 
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-app.get('/products', (req, res) => {
-  res.send(products)
-})
-app.get('/product/:productId', (req, res) => {
-  const id = req.params.productId;
-  res.json(products[id])
-})
+// app.get('/products', (req, res) => {
+//   res.send(products)
+// })
+// app.get('/product/:productId', (req, res) => {
+//   const id = req.params.productId;
+//   res.send(products[id])
+// })
 
 
 app.listen(port, () => {
